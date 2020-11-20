50a416d857a8bb9d2ac37ae64ffbde2b8450689c
harrygillingham99 <harrygillingham99@gmail.com>
2020-11-20 13:51:12 +0000

    feat(NSwag): added client gen script for store-api

---
+++ src/services/ClientBase.ts
@@ -0,0 +1,44 @@
+/**
+ * Base class for the client, provides the ability to add options to
+ * each server request as well as intercept responses.
+ */
+export class ClientBase {
+    private sessionToken?: string;
+    private clientAccess?: string;
+
+    public setToken(token: string): void {
+        this.sessionToken = token;
+    }
+
+    public setClientAccess(clientAccess: string): void {
+        this.clientAccess = clientAccess;
+    }
+
+    public clearToken(): void {
+        this.sessionToken = undefined;
+    }
+
+    protected transformOptions(opts: RequestInit): Promise<RequestInit> {
+        return Promise.resolve(opts);
+    }
+
+    protected transformResult<T>(
+        url: string,
+        response: Response,
+        processor: (resp: Response) => Promise<T>,
+    ): Promise<T> {
+        const result = processor(response);
+
+        return new Promise((res, rej) => {
+            return result
+                .then(data => {
+                    return res(data);
+                })
+                .catch(err => {
+                    return rej(err);
+                });
+        });
+    }
+}
+
+export default ClientBase;
--- src/state/AppState.ts
+++ src/state/AppState.ts
@@ -9,28 +9,13 @@ This is a global state container for the app.
 const useAppState = () => {
   const [user, setUser] = React.useState<firebase.User | undefined>();
   const [showAccountModal, toggleAccountModal] = React.useState<boolean>(false);
-  const [alert, setAlert] = React.useState<boolean>(false);
-  const [alertType, setAlertType] = React.useState<
-    "success" | "danger" | "primary"
-  >();
-  const ToggleAlert = (
-    show: boolean,
-    type?: "success" | "danger" | "primary"
-  ) => {
-    if (type !== undefined) {
-      setAlertType(type);
-    }
-    setAlert(show);
-  };
+
 
   return {
     user,
     setUser,
     showAccountModal,
-    toggleAccountModal,
-    alert,
-    alertType,
-    ToggleAlert,
+    toggleAccountModal
   };
 };
 
